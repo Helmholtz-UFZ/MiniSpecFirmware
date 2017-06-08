3617a53a361e155eccbb920c220109f7f077b9b1
bert palm <bert_palm@youbetterknow.it>
2017-06-08 17:12:51 +0200

    new directory struckture

---
+++ software_devl/micro_spec/Inc/helper_defines.h
@@ -0,0 +1,14 @@
+/*
+ * helper_defines.h
+ *
+ *  Created on: May 16, 2017
+ *      Author: Bert Palm
+ */
+
+#ifndef HELPER_DEFINES_H_
+#define HELPER_DEFINES_H_
+
+#define MAX(A,B)	((A) > (B) ? (A) : (B) )
+#define MIN(A,B)	((A) < (B) ? (A) : (B) )
+
+#endif /* HELPER_DEFINES_H_ */
