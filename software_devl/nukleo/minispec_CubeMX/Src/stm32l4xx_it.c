c64dd57d7f2ae2f9da3c987197f75fdad10fb4ae
Bert Palm <bert.palm@ufz.de>
2019-01-14 15:59:35 +0100

    still wip

--- software_devl/nukleo/minispec_CubeMX/Src/stm32l4xx_it.c
+++ software_devl/nukleo/minispec_CubeMX/Src/stm32l4xx_it.c
@@ -37,6 +37,10 @@
 
 /* USER CODE BEGIN 0 */
 
+#include "usart.h"
+#include "global_include.h"
+#include "micro_spec.h"
+
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
@@ -86,7 +90,7 @@ void SysTick_Handler(void)
   HAL_IncTick();
   HAL_SYSTICK_IRQHandler();
   /* USER CODE BEGIN SysTick_IRQn 1 */
-
+#define SysTick_Handler__OK
   /* USER CODE END SysTick_IRQn 1 */
 }
 
@@ -104,10 +108,47 @@ void EXTI2_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI2_IRQn 0 */
 
+	/*
+	 *
+	 * DATA READY TO READ - ADC1 BUSY
+	 * -------------------------------
+	 *
+	 * With every rising edge on the ADC-Busy-Line we come here and read the last value
+	 * of the ADC conversion. The value is read from the 16 GPIO pins, which are directly
+	 * connected to the parallel port of the ADC.
+	 *
+	 * This IR is only enabled [1] and disabled [2] in IRQHandlers.
+	 *
+	 * [1] TIM1_CC_IRQHandler()
+	 * [2] TIM1_CC_IRQHandler() or TIM5_IRQHandler().
+	 *
+	 * TODO (future release) use DMA instead of manually save values.
+	 */
+
+	uint8_t value0, value1;
+
+	// -------------- nomore code here !! --------------
+	if( __HAL_GPIO_EXTI_GET_IT(EXTADC1_BUSY_Pin) != RESET )
+	{
+		__HAL_GPIO_EXTI_CLEAR_IT( EXTADC1_BUSY_Pin );
+
+		if( sens1.data->wptr < (sens1.data->base + sens1.data->words) )
+		{
+			// read ADC parallel-port-value
+			value0 = GPIOA->IDR;
+			value1 = GPIOC->IDR;
+			*(sens1.data->wptr++) = (value1 << 8) | value0;
+		}
+	}
+	// -------------- nomore code here !! --------------
+
+#ifndef DO_NOT_USE_HAL_IRQ_HANDLER
+#error this_will_fail
   /* USER CODE END EXTI2_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
   /* USER CODE BEGIN EXTI2_IRQn 1 */
-
+#endif  /* DO_NOT_USE_HAL_IRQ_HANDLER */
+#define EXTI2_IRQHandler__OK
   /* USER CODE END EXTI2_IRQn 1 */
 }
 
@@ -132,10 +173,74 @@ void TIM1_CC_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM1_CC_IRQn 0 */
 
+	/*
+	 * CAPTURE DATA END - END OF SCAN (EOS)
+	 * ---------------------------------------
+	 *
+	 * Here we handle the capturing of the EOS of the sensor. EOS signal is send after
+	 * all pixel-data (samples) are send by the sensor. If we catch the signal we dis-
+	 * able the IR for the ADC-Busy-Line and return to the main program [1], which is
+	 * waiting for the status to change.
+	 *
+	 * [1] see sensor_wait_for_measurement_done() in micro_spec.c
+	 *
+	 */
+
+	if( (TIM1->SR & TIM_SR_CC2IF) && (TIM1->DIER & TIM_DIER_CC2IE) )
+	{
+		// clear IR flag
+		__HAL_TIM_CLEAR_IT( &htim1, TIM_IT_CC2 );
+
+		// Disable IR for ADC-busy-line. We also disable EOS,
+		// as we may got it early and we don't want to catch
+		// an other or many others.
+		NVIC_ClearPendingIRQ( EXTI2_IRQn );
+		NVIC_DisableIRQ( EXTI2_IRQn );
+
+		// do we got the eos to early ?
+		// this may happen if EOS is crosstalked by
+		// other signals. shielding (GND) it well.
+		if( TIM1->CCR2 < TRG_TO_EOS - 1 )
+		{
+			sens1.status = SENS_ERR_EOS_EARLY;
+		}
+		else
+		{
+			sens1.status = SENS_EOS_CAPTURED;
+		}
+		__HAL_TIM_DISABLE_IT( &htim1, TIM_IT_CC2 );
+	}
+
+	/*
+	 * CAPTURE DATA START
+	 * -------------------
+	 *
+	 * If we counted MSPARAM_CAPTURE_PXL_ST many TRG edges we start capturing the
+	 * values from the ADC. Here we enable the IR for the ADC-Busy-Line. To examine
+	 * the start of capturing, the TEST signal is set high at the same time
+	 * (automatically by the TIM1). One can read TEST on the oscilloscope.
+	 */
+	if( (TIM1->SR & TIM_SR_CC4IF) && (TIM1->DIER & TIM_DIER_CC4IE) )
+	{
+		// clear IR flag
+		__HAL_TIM_CLEAR_IT( &htim1, TIM_IT_CC4 );
+//		TIM1->SR &= ~TIM_SR_CC4IF;
+
+		// Enable IR for ADC-busy-line.
+		__HAL_GPIO_EXTI_CLEAR_IT( EXTADC1_BUSY_Pin );
+		NVIC_ClearPendingIRQ( EXTI2_IRQn );
+		NVIC_EnableIRQ( EXTI2_IRQn );
+
+		sens1.status = SENS_CAPTURE_DATA;
+	}
+
+#ifndef DO_NOT_USE_HAL_IRQ_HANDLER
+#error this_will_fail
   /* USER CODE END TIM1_CC_IRQn 0 */
   HAL_TIM_IRQHandler(&htim1);
   /* USER CODE BEGIN TIM1_CC_IRQn 1 */
-
+#endif  /* DO_NOT_USE_HAL_IRQ_HANDLER */
+#define TIM1_CC_IRQHandler__OK
   /* USER CODE END TIM1_CC_IRQn 1 */
 }
 
