82408c8c9c6323f5e9289eca74e49142674c6177
Bert Palm <bert.palm@ufz.de>
2019-01-16 17:01:45 +0100

    added serial-wire debug

--- software_devl/nukleo/minispec_CubeMX/Src/gpio.c
+++ software_devl/nukleo/minispec_CubeMX/Src/gpio.c
@@ -75,7 +75,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(POWER5V_SWITCH_ENBL_GPIO_Port, POWER5V_SWITCH_ENBL_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, ADC_RESET_Pin|ADC_PD_Pin|W5500_RST_Pin|W5500_SCS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, ADC_RESET_Pin|ADC_PD_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = Button_NucleoBoard_Pin;
@@ -121,8 +121,8 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIO_HeaderPin_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = ADC_RESET_Pin|ADC_PD_Pin|W5500_RST_Pin|W5500_SCS_Pin;
+  /*Configure GPIO pins : PAPin PAPin */
+  GPIO_InitStruct.Pin = ADC_RESET_Pin|ADC_PD_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
