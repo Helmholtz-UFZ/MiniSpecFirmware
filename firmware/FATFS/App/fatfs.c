b5367ea7b6ca6b8be086e49109558f4a0e88dc00
Bert Palm <bert.palm@ufz.de>
2019-03-06 19:32:14 +0100

    implemented get_fattime() for datetime on file access

--- firmware/FATFS/App/fatfs.c
+++ firmware/FATFS/App/fatfs.c
@@ -54,7 +54,7 @@ FATFS SDFatFS;    /* File system object for SD logical drive */
 FIL SDFile;       /* File object for SD */
 
 /* USER CODE BEGIN Variables */
-
+#include "rtc.h"
 /* USER CODE END Variables */    
 
 void MX_FATFS_Init(void) 
@@ -75,7 +75,25 @@ void MX_FATFS_Init(void)
 DWORD get_fattime(void)
 {
   /* USER CODE BEGIN get_fattime */
-  return 0;
+	RTC_TimeTypeDef sTime;
+	RTC_DateTypeDef sDate;
+
+	/* Always call GetDate after GetTime ! see HAL-documentation */
+	HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+	HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
+
+	/* ensure subtraction of 80 won't underflow 0.
+	 * Subtraction is neccesary by msdos timestamp format. */
+	uint8_t year = sDate.Year;
+	year = (year < 80) ? year + 20 : year - 80;
+
+	return ((DWORD) year << 25)
+			| ((DWORD) sDate.Month << 21)
+			| ((DWORD) sDate.Date << 16)
+			| ((DWORD) sTime.Hours << 11)
+			| ((DWORD) sTime.Minutes << 5)
+			| ((DWORD) sTime.Seconds >> 1);
+
   /* USER CODE END get_fattime */  
 }
 
