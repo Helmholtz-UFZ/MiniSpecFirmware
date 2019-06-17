05bbae91d250ce576bd9324b53f09ecb8e760b9c
Bert Palm <bert.palm@ufz.de>
2019-06-17 16:41:38 +0200

    mvd all NVIC_iR_EN to main, added GPIO IR for CMD_EN

--- firmware/Core/Inc/stm32l4xx_it.h
+++ firmware/Core/Inc/stm32l4xx_it.h
@@ -52,6 +52,7 @@ void EXTI2_IRQHandler(void);
 void DMA1_Channel5_IRQHandler(void);
 void TIM1_CC_IRQHandler(void);
 void USART1_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void RTC_Alarm_IRQHandler(void);
 void TIM5_IRQHandler(void);
 /* USER CODE BEGIN EFP */
